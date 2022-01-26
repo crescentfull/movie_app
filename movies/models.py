a6aba84c6deac42021c21a5a28e9d25f26cbc28e
song <song7868@gmail.com>
2022-01-26 22:48:48 +0900

    initial settings

--- movies/models.py
+++ movies/models.py
@@ -1,4 +1,9 @@
-from pyexpat import model
+from rest_framework import serializers
 from django.db import models
-
+from cores.models import Timestamp
 # Create your models here.
+class Movie(Timestamp.Model):
+    title = models.CharField(max_length=30)
+    genre = models.CharField(max_length=15)
+    year  = models.IntegerField()
+    
